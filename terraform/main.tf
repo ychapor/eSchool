713c6c88bafb627e121261f444d1f807f9512b39
Yura Chapor <yura.chapor@gmail.com>
2022-02-15 11:45:02 +0200

    Update Terrafotm backend configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = var.tf_rg_name
-    storage_account_name = var.tf_sa_name
-    container_name       = var.tf_c_name
-    key                  = var.tf_s_key
+    resource_group_name  = ""
+    storage_account_name = ""
+    container_name       = ""
+    key                  = ""
   }
 }
 
@@ -23,12 +23,4 @@ provider "azurerm" {
 resource "azurerm_resource_group" "app_rg" {
   name     = var.app_rg_name
   location = var.app_rg_location
-}
-
-# Create a virtual network within the resource group
-resource "azurerm_virtual_network" "eschool_vnet" {
-  name                = "eschool-vnet"
-  resource_group_name = azurerm_resource_group.app_rg.name
-  location            = azurerm_resource_group.app_rg.location
-  address_space       = ["10.0.0.0/16"]
 }
